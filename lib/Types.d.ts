c957de3a89a3c634acb7782311ff2aff22c185c7
AdrianAndersen <adrianandersen@protonmail.com>
2021-07-06 15:46:54 +0200

    Add basic file structure and components

---
+++ lib/Types.d.ts
@@ -0,0 +1,37 @@
+// A common storage for the types used in the app
+
+export type UserRole = "customer" | "insurer" | "police" | "transportCompany";
+
+export type ID = number;
+
+export type User = {
+  id: ID;
+  name: string;
+  email: string;
+  role: UserRole;
+};
+
+export type Item = {
+  id: ID;
+  name: string;
+  description: string;
+  imageIDs: string[];
+  bounty: number;
+  status: string;
+  lostAt?: string;
+  lostDate?: string;
+  expirationDate: string;
+  owners: ID[];
+};
+
+export type GlobalState = {
+  currentUser?: User;
+  items?: Item[];
+};
+
+export type DispatchAction = "SET_STATE" | "SET_CURRENT_USER";
+
+export type DispatchObject = {
+  type: DispatchAction;
+  payload: any;
+};
