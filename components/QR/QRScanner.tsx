cc3c27486621ffb996a40142c354f13a99232c04
Adrian Andersen <adrianandersen@protonmail.com>
2021-07-12 18:55:10 +0200

    Implement QR search functionality (#9)

--- components/QR/QRScanner.tsx
+++ components/QR/QRScanner.tsx
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { BarCodeScanner, BarCodeScannerResult } from "expo-barcode-scanner";
+import { fetchItem } from "../../lib/requests";
+import { Item } from "../../lib/Types";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
+    width: "100%",
   },
 });
 
-export default function QRScanner() {
+export default function QRScanner({
+  setItem,
+  createItem,
+}: {
+  // eslint-disable-next-line no-unused-vars
+  setItem: (item: Item) => void;
+  createItem: boolean;
+}) {
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [scanned, setScanned] = useState(false);
 
@@ -21,10 +31,25 @@ export default function QRScanner() {
     })();
   }, []);
 
-  const handleBarCodeScanned = ({ type, data }: BarCodeScannerResult) => {
+  const handleBarCodeScanned = async ({ data }: BarCodeScannerResult) => {
+    const matcher = data.match(/\/(\S+)/);
+    if (!matcher) {
+      // eslint-disable-next-line no-alert
+      alert("Ugyldig lenke!");
+      return;
+    }
+    const itemID = Number(matcher[1]);
+    let item = await fetchItem(itemID);
+    if (!item) {
+      if (!createItem) {
+        // eslint-disable-next-line no-alert
+        alert("Fant ikke gjenstand!");
+        return;
+      }
+      item = { id: itemID } as Item;
+    }
     setScanned(true);
-    // eslint-disable-next-line no-alert
-    alert(type + data);
+    setItem(item);
   };
 
   if (hasPermission === null) {
--- screens/ItemOverview.tsx
+++ screens/ItemOverview.tsx
@@ -1,29 +1,63 @@
 import * as React from "react";
 
-import { Button, Heading, HStack, View } from "native-base";
+import { NavigationProp } from "@react-navigation/native";
+import { Button, Heading, HStack, Icon, IconButton, View } from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import { useState } from "react";
 import { useGlobalState } from "../components/StateManagement/GlobalState";
 import ItemCard from "../components/Item/ItemCard";
 import { isAdmin } from "../lib/helpers";
+import QRScanner from "../components/QR/QRScanner";
+import { Item } from "../lib/Types";
 
-export default function ItemOverview() {
+export default function ItemOverview({
+  navigation,
+}: {
+  navigation: NavigationProp<any>;
+}) {
   const { state } = useGlobalState();
+  const [scannerOpen, setScannerOpen] = useState(false);
+  const [scannedItem, setScannedItem] = useState<Item>();
+  const setItem = (i: Item): void => {
+    setScannerOpen(false);
+    setScannedItem(i);
+  };
+
+  const scanQR = () => {
+    setScannerOpen(true);
+  };
+
+  const getItemList = () => {
+    const itemList = scannedItem ? [scannedItem] : state.items;
+    return itemList?.map((i) => <ItemCard key={i.id} item={i} />);
+  };
 
   return (
     <View flex={1} alignItems="center" overflow="scroll">
       <Heading mb={5}>
         {isAdmin(state.currentUser) ? "Alle gjenstander" : "Mine gjenstander"}
       </Heading>
-      {state.items?.map((item) => (
-        <ItemCard key={item.id} item={item} />
-      ))}
-      {isAdmin(state.currentUser) && (
+      {scannerOpen && <QRScanner createItem={false} setItem={setItem} />}
+      {!scannerOpen && !scannedItem && isAdmin(state.currentUser) && (
         <>
-          <Heading>Finn gjenstand</Heading>
-          <HStack mt={2}>
-            <Button mr={5}>QR</Button>
+          <Heading size="lg">Finn gjenstand</Heading>
+          <HStack mt={2} mb={5}>
+            <Button mr={5} onPress={scanQR}>
+              QR
+            </Button>
             <Button>NFC</Button>
           </HStack>
         </>
+      )}
+      {scannedItem && (
+        <Button onPress={() => setScannedItem(undefined)}>Fjern filter</Button>
+      )}
+      {!scannerOpen && getItemList()}
+      {!scannerOpen && !isAdmin(state.currentUser) && (
+        <IconButton
+          onPress={() => navigation.navigate("Ny gjenstand")}
+          icon={<Icon size="sm" as={<AntDesign name="plus" />} />}
+        />
       )}
     </View>
   );
